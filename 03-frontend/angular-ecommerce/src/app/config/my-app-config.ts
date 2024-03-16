79af6a3129e3c3272a7052cfc28a1508b37a9d0f
adityabeniwal <93305298+adityabeniwal@users.noreply.github.com>
2024-03-16 17:30:27 +0530

    g
    d

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oafu2ktmeMMQXGNs5d7',
+        issuer: 'https://dev-42615270.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
